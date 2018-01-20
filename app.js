f9e775a53a2da2559c1dbe6c6e337581f1c0d9b5
Joe Stowers <greenchiletexan@gmail.com>
2018-01-20 00:10:20 -0600

    app: add Node env variable for PORT

--- app.js
+++ app.js
@@ -8,15 +8,12 @@ const bodyParser = require('body-parser');
 // routes is a function()
 const routes = require('./routes/routes');
 
-
 /*
 	app is an object that takes in requests, and
 	depending on the request will run different code
 */
 const app = express();
-
-
-
+const PORT = process.env.PORT || 3000;
 
 // any incoming request, assume it is json, and parse into an object
 // bodyParser must be placed above routes(app)
@@ -29,14 +26,15 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	next();
 });
+
 routes(app);
 
 
-// listen for incoming HTTP requests on port 3050
+// listen for incoming HTTP requests on specified port
 // added if statement so mocha test would run without error
 if(!module.parent) {
-	app.listen(3050, () => {
-		console.log('Running on port 3050');
+	app.listen(PORT, () => {
+		console.log('Server is listening on port', PORT);
 	});
 }
 
