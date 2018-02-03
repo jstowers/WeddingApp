49d8dcc09cbfe2d8c9b9ad05f8d41b15115bf76a
Joe Stowers <greenchiletexan@gmail.com>
2018-02-03 01:15:28 -0600

    Add es6-promise-promise polyfill for Promise to work on Internet Explorer

--- src/components/RSVPForm.jsx
+++ src/components/RSVPForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Promise from 'es6-promise-promise';
 import axios from 'axios';
 import { Form,
 		 Col,
--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,9 @@ const config = {
         }),
         new webpack.DefinePlugin({
             __API__: apiHost
+        }),
+        new webpack.ProvidePlugin({
+            Promise: 'es6-promise-promise'
         })
     ]
 }
