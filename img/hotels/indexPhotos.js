aca15f030ee1ed5636e1cfaf9d822e82de4a817d
Joe Stowers <greenchiletexan@gmail.com>
2018-02-08 21:48:45 -0600

    YourStay: update page with new hotels

---
+++ img/hotels/indexPhotos.js
@@ -0,0 +1,15 @@
+import NativoLodge from './NativoLodge.jpg';
+import MarriottPyramid from './MarriottPyramid.jpg';
+import CourtyardMarriott from './CourtyardMarriott.jpg';
+import StaybridgeSuites from './StaybridgeSuites.jpg';
+import SandiaCasino from './SandiaCasino.jpg';
+import HyattTamaya from './HyattTamaya.jpg';
+
+export default { 
+	NativoLodge, 
+	MarriottPyramid,
+	CourtyardMarriott,
+	StaybridgeSuites,
+	SandiaCasino,
+	HyattTamaya
+};
--- src/components/YourStay.jsx
+++ src/components/YourStay.jsx
@@ -4,9 +4,43 @@
 import React, { Component } from 'react';
 import { Grid, Col, Row } from 'react-bootstrap';
 import yourstayStyle from '../../style/06-yourstay.css';
-import { NativoLodge } from '../../img/hotels/indexPhotos';
+import { 
+	NativoLodge, 
+	MarriottPyramid,
+	CourtyardMarriott,
+	StaybridgeSuites,
+	SandiaCasino,
+	HyattTamaya
+} from '../../img/hotels/indexPhotos';
 
 const hotelsArray = [
+	{
+		name: 'Marriott Pyramid',
+		img: '/images/MarriottPyramid.jpg',
+		url: 'http://bit.ly/19Swcb8',
+		address1: '5151 San Francisco Rd. NE',
+		address2: 'Albuquerque, NM 87109',
+		phone: '505-821-3333',
+		distance: '4.0 miles'
+	},
+	{
+		name: 'Courtyard Albuquerque',
+		img: '/images/CourtyardMarriott.jpg',
+		url: 'http://bit.ly/2GLve8H',
+		address1: '5151 Journal Center Blvd. NE',
+		address2: 'Albuquerque, NM 87109',
+		phone: '505-823-1919',
+		distance: '4.0 miles'
+	},
+	{
+		name: 'Staybridge Suites North',
+		img: '/images/StaybridgeSuites.jpg',
+		url: 'http://bit.ly/2BV9ELa',
+		address1: '5817 Signal Ave. NE',
+		address2: 'Albuquerque, NM 87113',
+		phone: '505-266-7829',
+		distance: '4.7 miles'
+	},
 	{
 		name: 'Nativo Lodge',
 		img: 'images/NativoLodge.jpg',
@@ -17,40 +51,22 @@ const hotelsArray = [
 		distance: '6.0 miles'
 	},
 	{
-		name: 'Marriott Pyramid',
-		img: '/images/NativoLodge.jpg',
-		url: 'http://bit.ly/19Swcb8',
-		address1: '5151 San Francisco Rd. NE',
-		address2: 'Albuquerque, NM 87109',
-		phone: '505-821-3333',
-		distance: '4.0 miles'
-	},
-	{
-		name: 'Courtyard Albuquerque',
-		img: '/images/NativoLodge.jpg',
-		url: 'http://bit.ly/2GLve8H',
-		address1: '5151 Journal Center Blvd. NE',
-		address2: 'Albuquerque, NM 87109',
-		phone: '505-823-1919',
-		distance: '4.0 miles'
-	},
-	{
-		name: 'Staybridge Suites North',
-		img: '/images/NativoLodge.jpg',
-		url: 'http://bit.ly/2BV9ELa',
-		address1: '5817 Signal Ave. NE',
-		address2: 'Albuquerque, NM 87113',
-		phone: '505-266-7829',
-		distance: '4.7 miles'
-	},
-	{
 		name: 'Sandia Resort & Casino',
-		img: '/images/NativoLodge.jpg',
+		img: '/images/SandiaCasino.jpg',
 		url: 'http://www.sandiacasino.com/hotel/',
 		address1: '30 Rainbow Rd. NE',
 		address2: 'Albuquerque, NM 87113',
 		phone: '505-796-7500',
 		distance: '6.3 miles'
+	},
+	{
+		name: 'Hyatt Regency Tamaya Resort & Spa',
+		img: '/images/HyattTamaya.jpg',
+		url: 'http://bit.ly/2k76vCY',
+		address1: '1300 Tuyuna Trail',
+		address2: 'Santa Ana Pueblo, NM 87004',
+		phone: '505-867-1234',
+		distance: '16.0 miles'
 	}
 ]
 
